d7259a39f9266cee73fe51503e5be1d539e0700c
Roberto Pisu <roberto.pisu03@gmail.com>
2024-02-28 21:41:57 +0100

    added a folder for Esercitazione2

--- Esercitazione3/es3.py
+++
@@ -1,9 +0,0 @@
-user_list = input("insert a list that elements are separeted with comma or space\n")
-elements = user_list.split()
-
-elements = [int(element) for element in elements]
-new_list = []
-for a in elements:
-    if elements.index(a) % 2 == 0:
-        new_list.append(a)
-print(new_list)
